ad6b48df6ff22327a38c6930a15814c88ec48f3b
Cypher2012 <cypher2012@hotmail.co.uk>
2017-01-13 17:15:58 +0000

    Add files via upload

--- ExtendedCharacterMovement.h
+++ ExtendedCharacterMovement.h
@@ -10,23 +10,32 @@ class HDREMAKE_V4_API UExtendedCharacterMovement : public UCharacterMovementComp
 private:
 
 	bool bIsTransitioning = false;
+
 	bool bIsProne = false;
+	bool bIsCrouched = false;	
 	bool bIsStanding = true;
 
 	virtual float GetMaxSpeed() const override;
 
 public:
-
+	
 	UPROPERTY(Category = "Character Movement: Walking", EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0", UIMin = "0"))
 	float MaxProneSpeed;
-	
-	//Sets the IsProne value to true. Adjusts the pawns collision capsule
-	UFUNCTION(BlueprintCallable, category = "Custom movement")
-	void Prone(bool bClientSimulation);
 
 	//Sets the IsProne value to false. Adjusts the pawns collision capsule
+	//See IsStanding
 	UFUNCTION(BlueprintCallable, category = "Custom movement")
-	void StandUp(bool bClientSimulation);
+	void StandUp(bool bClientSimulation = false);
+
+	//Sets the IsCrouched value to true. Adjusts the pawns collision capsule
+	//See IsCrouched
+	UFUNCTION(BlueprintCallable, category = "Custom movement")
+	virtual void Crouch(bool bClientSimulation = false)  override;
+	
+	//Sets the IsProne value to true. Adjusts the pawns collision capsule
+	//See IsProne
+	UFUNCTION(BlueprintCallable, category = "Custom movement")
+	void Prone(bool bClientSimulation = false);	
 
 	//Typically used by animation notify events.
 	//Used to tell the movement component that it's in the middle of a transition and we don't want the character to move until the EndTransition() function is called.
@@ -37,4 +46,18 @@ public:
 	//It will tell the movement component that the transition is finished and the character is free to move again
 	UFUNCTION(BlueprintCallable, category = "Custom movement")
 	void EndTransition();
+
+	//GetMethods
+
+	//Return bIsStanding
+	UFUNCTION(BlueprintCallable, category = "Custom movement")
+	bool IsStanding() { return bIsStanding; }
+
+	//Return bIsCrouched
+	UFUNCTION(BlueprintCallable, category = "Custom movement")
+	bool IsCrouched() { return bIsCrouched; }
+
+	//Return bIsProne
+	UFUNCTION(BlueprintCallable, category = "Custom movement")
+	bool IsProne() { return bIsProne; }
 };
