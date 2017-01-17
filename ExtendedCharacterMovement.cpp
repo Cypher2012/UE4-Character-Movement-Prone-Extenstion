c1103de3683024a36df546fb92a8276ea0121ced
Cypher2012 <cypher2012@hotmail.co.uk>
2017-01-17 10:54:46 +0000

    Update ExtendedCharacterMovement.cpp

--- ExtendedCharacterMovement.cpp
+++ ExtendedCharacterMovement.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "HDRemake_V4.h"
+#include "project_name.h"
 #include "ExtendedCharacterMovement.h"
 
 UExtendedCharacterMovement::UExtendedCharacterMovement()
